e9798941fc07814337a756ba443fddddf7fbd3b0
Matija Cetrovic <matijacetrovic@gmail.com>
2020-11-14 22:35:11 +0100

    alfa 1

--- ConsoleApplication97/ConsoleApplication97.cpp
+++ ConsoleApplication97/ConsoleApplication97.cpp
@@ -1,28 +1,33 @@
 #include "tetris.h"
 
-#include <iostream>
-#include <thread>
-#include <chrono>
-
 bool game = true;
 bool keys[4];
 bool falling = false;
+bool flevel = true;
 
-short x, y, max_x, min_x, max_y;
+int pice = 0;
+int next_pice;
+
+short x, y, max_x, min_x, max_y, tlines;
+
+int speed[30] = { 18,16,14,12,10,8,6,6,5,5,5,4,4,4,4,3,3,3,3,2,2,2,2,2,2,2,2,2,2,1 };
+int level = 0;
+int frame = 0;
+
+int points[5] = { 0,40,100,300,1200 };
+int score = 0;
+
+int lines = 0;
+int mlines;
 
 int main()
 {
 	tetris igra;
-	
-	igra.inicijalizacija();
 
-	ShowCursor(false);
+	next_pice = igra.inicijalizacija();
 
 	while (game)
 	{
-		//timing-------------
-		std::this_thread::sleep_for(std::chrono::microseconds(16700));
-
 		//input--------------
 		for (int i = 0; i < 4; i++)
 		{
@@ -30,46 +35,101 @@ int main()
 		}
 
 		//testing------------
+			//stvara novi oblik
 		if (!falling)
 		{
 			falling = true;
 			igra.zamena();
-			igra.odredjivanje_oblika();
+			pice = next_pice;
+			next_pice = igra.odredjivanje_oblika();
 			x = 3;
 			y = -1;
-			
+
 			if (!igra.provera(x, y))
 				game = false;
 		}
 
-		if (keys[0])
+		//levo
+		if (keys[0] && x - 1 >= igra.min_x() && igra.provera(x - 1, y))
 		{
-			if(x-1 >= igra.min_x())
-				x -= 1;
+			x -= 1;
 		}
-		else if (keys[2])
+		//desno
+		else if (keys[2] && x + 1 <= igra.max_x() && igra.provera(x + 1, y))
 		{
-			if(x+1 <= igra.max_x())
-				x += 1;
+			x += 1;
 		}
-		
-		if (keys[3] && ((y + 1) < 20))
-			y += 1;
 
-		/*if (keys[1])
+		//dole
+		if (keys[3] && y + 1 <= igra.max_y() && igra.provera(x, y + 1))
 		{
-			igra.rotacija();
-			bool test = igra.provera(x, y);
-			if (!test)
+			y += 1;
+		}
+
+		//rotacija
+		if (keys[1])
+		{
+			igra.rotacija(pice);
+			if (!igra.provera(x, y) || x > igra.max_x() || x<igra.min_x() || y>igra.max_y())
 			{
-				igra.rotacija();
-				igra.rotacija();
-				igra.rotacija();
+				igra.rotacija(pice);
+				igra.rotacija(pice);
+				igra.rotacija(pice);
 			}
-		}*/
-			
+		}
+
+		//ako je level veci od 29
+		int t = level;
+		if (t > 29)
+			t = 29;
+
+		//tajming
+		if (frame == speed[t])
+		{
+			//provera mogucnosti spustanja oblika
+			frame = -1;
+			if (igra.provera(x, y + 1) && y + 1 <= igra.max_y())
+			{
+				y += 1;
+			}
+			else
+			{
+				//dobijanje novog oblika
+				falling = false;
+				//brisanje starog oblika
+				igra.stapanje(x, y);
+				//dobijanje sastavljenih linija ako ih ima i njihovo brisanje sa table
+				tlines = igra.nDlines();
+
+				//uvecavanje poena
+				score += points[tlines] * (level + 1);
+				//uvecavanje broja unistenih linija
+				lines += tlines;
+
+				//da li je trenutni level pocetni level
+				if (flevel)
+				{
+					//uslov za uvecavanje pocetnog levela levela
+					if (lines >= level * 10 + 10 || lines >= max(100, (level * 10 - 50)))
+					{
+						flevel = false;
+						level++;
+						mlines = lines;
+					}
+				}
+				//uslov za uvecavanje ostalih levela
+				else if (lines - mlines >= 10)
+				{
+					mlines = lines;
+					level++;
+				}
+			}
+		}
+
 		//output-------------
-		igra.stampaj(x, y);
+		igra.stampaj(x, y, score, level);
+
+		frame++;
 	}
 	return 0;
 }
--- ConsoleApplication97/tetris.cpp
+++ ConsoleApplication97/tetris.cpp
@@ -3,89 +3,111 @@
 
 #include "tetris.h"
 
-void tetris::rotacija()
-{
-	int Tetris[4][4];
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			int poz = i - (n - 1);
-			if (poz < 0)
-				poz *= -1;
-			Tetris[j][poz] = tetronim[i][j];
-		}
-	}
-	for (int i = 0; i < n * n; i++)
-	{
-		tetronim[i / n][i % n] = Tetris[i / n][i % n];
-	}
-}
-
-void tetris::pos_rotacija()
-{
-	int Tetris[4][4];
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			int poz = i - (n - 1);
-			if (poz < 0)
-				poz *= -1;
-			Tetris[j][poz] = next[i][j];
-		}
-	}
-	for (int i = 0; i < n * n; i++)
-	{
-		next[i / n][i % n] = Tetris[i / n][i % n];
-	}
-}
-
-void tetris::odredjivanje_oblika()
+//nalazenje sledeceg oblika (next)
+int tetris::odredjivanje_oblika()
 {
 	srand(time(NULL));
 
 	int pice = rand() % 7;
 
-	for (int i = 0; i < n * n; i++)
+	for (int i = 0; i < 4 * 4; i++)
 	{
-		next[i / n][i % n] = pices[pice][i / n][i % n];
+		next[i / 4][i % 4] = pices[pice][i / 4][i % 4];
 	}
 
 	srand(time(NULL));
 	int ponavljanje = rand() % 4;
 	for (int i = 0; i < ponavljanje; i++)
-		pos_rotacija();
+		pos_rotacija(pice);
+	return pice;
 }
 
+//tetronim = next
 void tetris::zamena()
 {
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < 4; i++)
 	{
-		for (int j = 0; j < n; j++)
+		for (int j = 0; j < 4; j++)
 		{
 			tetronim[i][j] = next[i][j];
 		}
 	}
 }
 
+//rotacija trenutnog oblika (tetronim)
+void tetris::rotacija(int pice)
+{
+	int Tetris[4][4] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
+
+	int k = 3;
+	int p = pice;
+	if (p == 0 || p == 2)
+	{
+		k = 4;
+	}
+
+	for (int i = 0; i < k; i++)
+	{
+		for (int j = 0; j < k; j++)
+		{
+			int poz = i - (k - 1);
+			if (poz < 0)
+				poz *= -1;
+			Tetris[j][poz] = tetronim[i][j];
+		}
+	}
+	for (int i = 0; i < 4 * 4; i++)
+	{
+		tetronim[i / 4][i % 4] = Tetris[i / 4][i % 4];
+	}
+}
+
+//rotacija sledeceg oblika (next)
+void tetris::pos_rotacija(int pice)
+{
+	int Tetris[4][4] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
+
+	int k = 3;
+	int p = pice;
+	if (p == 0 || p == 2)
+	{
+		k = 4;
+	}
+
+	for (int i = 0; i < k; i++)
+	{
+		for (int j = 0; j < k; j++)
+		{
+			int poz = i - (k - 1);
+			if (poz < 0)
+				poz *= -1;
+			Tetris[j][poz] = next[i][j];
+		}
+	}
+	for (int i = 0; i < 4 * 4; i++)
+	{
+		next[i / 4][i % 4] = Tetris[i / 4][i % 4];
+	}
+}
+
+//da li se oblik uklapa na zadate kordinate
 bool tetris::provera(int x, int y)
 {
-	int i = 0;
-	int j = 0;
+	int k = 0;
+	int l = 0;
 
 	if (y < 0)
 	{
-		i = -y;
+		k = -y;
 	}
 	if (x < 0)
 	{
-		j = -x;
+		l = -x;
 	}
 
-	for (i = i; i < n; i++)
+	for (int i = k; i < 4; i++)
 	{
-		for (j = j; j < n; j++)
+		for (int j = l; j < 4; j++)
 		{
 			if (tabla[i + y][j + x] != 0 && tetronim[i][j] != 0)
 			{
@@ -96,72 +118,170 @@ bool tetris::provera(int x, int y)
 	return true;
 }
 
+//maximalno x za dati oblik
 int tetris::max_x()
 {
 	int Max_x = 9;
-	for (int j = 0; j < n; j++)
+	for (int j = 0; j < 4; j++)
 	{
-		for (int i = 0; i < n; i++)
+		for (int i = 0; i < 4; i++)
 		{
-			if (tetronim[i][j] == 1 && 9-i<Max_x)
+			if (tetronim[i][j] == 1 && 9 - j < Max_x)
 			{
-				Max_x = 9 - i;
+				Max_x = 9 - j;
 			}
 		}
 	}
 	return Max_x;
 }
 
+//minimalno x za dati oblik
 int tetris::min_x()
 {
-	int Min_x = 0;
-	for (int j = 0; j < n; j++)
+	int Min_x = -3;
+	for (int j = 0; j < 4; j++)
 	{
-		for (int i = 0; i < n; i++)
+		for (int i = 0; i < 4; i++)
 		{
-			if (tetronim[i][j] == 1 && 0 - i > Min_x)
+			if (tetronim[i][j] == 1 && 0 - j > Min_x)
 			{
-				Min_x = - i;
+				Min_x = -j;
 			}
 		}
 	}
 	return Min_x;
 }
 
-void tetris::stampaj(int x, int y)
+//maximalno x za dati oblik
+int tetris::max_y()
+{
+	int Max_y = 19;
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			if (tetronim[i][j] == 1 && 19 - i < Max_y)
+			{
+				Max_y = 19 - i;
+			}
+		}
+	}
+	return Max_y;
+}
+
+//pretvaranje trenutnog oblika u pozadinu
+void tetris::stapanje(int x, int y)
+{
+	for (int i = 0;i < 4;i++)
+	{
+		for (int j = 0;j < 4;j++)
+		{
+			if (tetronim[i][j] == 1)
+			{
+				tabla[y + i][x + j] = 1;
+			}
+		}
+	}
+}
+
+//dobijanje broja unistenih linija
+int tetris::nDlines()
+{
+	bool test;
+	int num_lines = 0;
+	for (int i = 0;i < 20;i++)
+	{
+		test = true;
+		for (int j = 0;j < 10;j++)
+		{
+			if (tabla[i][j] == 0)
+			{
+				test = false;
+				break;
+			}
+		}
+		if (test)
+		{
+			num_lines += 1;
+			for (int k = i;k > 0;k--)
+			{
+				for (int l = 0;l < 10;l++)
+				{
+					tabla[k][l] = tabla[k - 1][l];
+				}
+			}
+			for (int l = 0;l < 10;l++)
+			{
+				tabla[0][l] = 0;
+			}
+		}
+	}
+	return num_lines;
+}
+
+//ispis
+void tetris::stampaj(int x, int y, int score, int level)
 {
 	system("cls");
-	std::cout << std::endl;
+	std::cout << "\033[0;32m" << std::endl;
 	for (int i = 0; i < 21; i++)
 	{
 		for (int j = 0; j < 17; j++)
 		{
 			if ((i == 0 && j < 5) || (j == 0 && i < 5) || (i == 5 && j < 5) || j == 5 || j == 16 || (i == 20 && j > 5 && j < 16))
-				std::cout << "#";
+			{
+				std::cout << char(178) << char(178);
+				if (j == 16)
+				{
+					if (i == 0)
+					{
+						std::cout << "   SCORE:";
+					}
+					else if (i == 1)
+					{
+						std::cout << "   " << score;
+					}
+					else if (i == 3)
+					{
+						std::cout << "   LEVEL:";
+					}
+					else if (i == 4)
+					{
+						std::cout << "   " << level;
+					}
+				}
+			}
 			else if ((i > 0 && i < 5) && (j > 0 && j < 5))
 			{
 				if (next[i - 1][j - 1] == 1)
-					std::cout << 'X';
+					std::cout << char(219) << char(219);
 				else
-					std::cout << ' ';
+					std::cout << "  ";
 			}
-			else if ((i > y - 1 && i < y + 4) && (j - 6 > x - 1 && j - 6 < x + 4))
+			else if ((i > y - 1 && i < y + 4) && (j - 6 > x - 1 && j - 6 < x + 4) && tetronim[i - y][j - x - 6] == 1)
 			{
-				if (tetronim[i - y][j - x - 6] == 1)
-					std::cout << 'X';
-				else
-					std::cout << ' ';
+				std::cout << char(219) << char(219);
 			}
-			else std::cout <<" ";
+			else if (i >= 0 && i < 20 && j>5 && j < 16)
+			{
+				if (tabla[i][j - 6] == 1)
+					std::cout << char(219) << char(219);
+				else
+					std::cout << " .";
+			}
+
+			else std::cout << "  ";
 		}
 		std::cout << std::endl;
 	}
 }
 
-void tetris::inicijalizacija()
+//set funkcija koje se moraju pozvati pre pocetka igre
+int tetris::inicijalizacija()
 {
-	odredjivanje_oblika();
+	int pice = odredjivanje_oblika();
 	std::this_thread::sleep_for(std::chrono::seconds(1));
+	return pice;
 }
 
 #endif;
